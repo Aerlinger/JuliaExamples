c47543856d3c5be54464abf5b2759a5d96b8e038
Anthony Erlinger <aerlinger@gmail.com>
2014-04-19 22:11:36 -0400

    Add modules.md

---
+++ Modules.jl
@@ -0,0 +1,19 @@
+module Modules
+
+  #push!(LOAD_PATH, "/Path/To/My/Module/")
+
+  """
+    Workspaces for global variables
+  """
+
+  # include ...
+  # import ...
+  # importall
+  # export ...
+  # using
+
+  using FactCheck
+
+  facts("Modules") do
+  end
+end
